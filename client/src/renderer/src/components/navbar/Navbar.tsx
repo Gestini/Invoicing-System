1ee8ed8fe8779955e07c3475d140b0eab82dd8e7
nicoluu <estebaanlunaaa@gmail.com>
2024-06-25 19:18:42 -0300

    style: ✔darkmode

--- client/src/renderer/src/components/navbar/Navbar.tsx
+++ client/src/renderer/src/components/navbar/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Sidebar({ children }: SidebarProps) {
 
   return (
     <aside className="flex min-h-[100vh]">
-      <nav className="h-full  flex flex-col bg-white border-r shadow-sm">
+      <nav className="h-full  flex flex-col border-r shadow-sm">
         <div className="p-4 pb-2 flex justify-between items-center">
           <img
             src="https://img.logoipsum.com/243.svg"
