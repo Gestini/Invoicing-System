83f9872af8b53b65f1d84a54de1484663571d62a
josessoria <josessoriap21@gmail.com>
2024-06-25 20:37:41 -0300

    fix: 🐛 Addtableproducts

--- api/src/main/java/productar/models/ProductModel.java
+++ api/src/main/java/productar/models/ProductModel.java
@@ -26,12 +26,39 @@ public class ProductModel {
     private Long id;
 
     @Column
+    private String codigo1;
+
+    @Column
+    private String codigo2;
+
+    @Column
+    private String barcode;
+
+    @Column
     private String image;
 
     @Column(nullable = false)
     @NotNull(message = "Price cannot be null")
     @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than zero")
     private Double price;
+
+    @Column
+    private Double cardPrice;
+
+    @Column
+    private Double financedPrice;
+
+    @Column
+    private Double friendPrice;
+
+    @Column
+    private Double purchasePrice;
+
+    @Column
+    private Double priceCalculation;
+
+    @Column
+    private Double costPrice;
 
     @Column(nullable = false)
     @NotNull(message = "Quantity cannot be null")
@@ -66,28 +93,176 @@ public class ProductModel {
     @NotNull(message = "Business unit cannot be null")
     private BusinessUnitsModel businessUnit;
 
-    
-    private String codigo1;
-    private String codigo2;
-    private String barcode;
-    private String categoryType;
-    private Double purchasePrice;
-    private Double priceCalculation;
-    private Double costPrice;
+    @Column
     private String pricePolicy;
+
+    @Column
     private Double net1;
+
+    @Column
     private Double net2;
+
+    @Column
     private Double net3;
+
+    @Column
     private Double net4;
+
+    @Column
     private String taxType;
-    private Double cashPrice;
-    private Double cardPrice;
-    private Double financedPrice;
-    private Double friendPrice;
+
+    @Column
     private String referenceCode;
+
+    @Column
     private Boolean packageProduct;
+
+    @Column
     private Integer quantityPerPackage;
-    private String observations;
+
+    public String getCodigo1() {
+        return codigo1;
+    }
+
+    public void setCodigo1(String codigo1) {
+        this.codigo1 = codigo1;
+    }
+
+    public String getCodigo2() {
+        return codigo2;
+    }
+
+    public void setCodigo2(String codigo2) {
+        this.codigo2 = codigo2;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public Double getCardPrice() {
+        return cardPrice;
+    }
+
+    public void setCardPrice(Double cardPrice) {
+        this.cardPrice = cardPrice;
+    }
+
+    public Double getFinancedPrice() {
+        return financedPrice;
+    }
+
+    public void setFinancedPrice(Double financedPrice) {
+        this.financedPrice = financedPrice;
+    }
+
+    public Double getFriendPrice() {
+        return friendPrice;
+    }
+
+    public void setFriendPrice(Double friendPrice) {
+        this.friendPrice = friendPrice;
+    }
+
+    public Double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(Double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public Double getPriceCalculation() {
+        return priceCalculation;
+    }
+
+    public void setPriceCalculation(Double priceCalculation) {
+        this.priceCalculation = priceCalculation;
+    }
+
+    public Double getCostPrice() {
+        return costPrice;
+    }
+
+    public void setCostPrice(Double costPrice) {
+        this.costPrice = costPrice;
+    }
+
+    public String getPricePolicy() {
+        return pricePolicy;
+    }
+
+    public void setPricePolicy(String pricePolicy) {
+        this.pricePolicy = pricePolicy;
+    }
+
+    public Double getNet1() {
+        return net1;
+    }
+
+    public void setNet1(Double net1) {
+        this.net1 = net1;
+    }
+
+    public Double getNet2() {
+        return net2;
+    }
+
+    public void setNet2(Double net2) {
+        this.net2 = net2;
+    }
+
+    public Double getNet3() {
+        return net3;
+    }
+
+    public void setNet3(Double net3) {
+        this.net3 = net3;
+    }
+
+    public Double getNet4() {
+        return net4;
+    }
+
+    public void setNet4(Double net4) {
+        this.net4 = net4;
+    }
+
+    public String getTaxType() {
+        return taxType;
+    }
+
+    public void setTaxType(String taxType) {
+        this.taxType = taxType;
+    }
+
+    public String getReferenceCode() {
+        return referenceCode;
+    }
+
+    public void setReferenceCode(String referenceCode) {
+        this.referenceCode = referenceCode;
+    }
+
+    public Boolean getPackageProduct() {
+        return packageProduct;
+    }
+
+    public void setPackageProduct(Boolean packageProduct) {
+        this.packageProduct = packageProduct;
+    }
+
+    public Integer getQuantityPerPackage() {
+        return quantityPerPackage;
+    }
+
+    public void setQuantityPerPackage(Integer quantityPerPackage) {
+        this.quantityPerPackage = quantityPerPackage;
+    }
 
     public Long getId() {
         return id;
