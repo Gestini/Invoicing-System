b3807e378febafc1ba887c7937972bd6af83af6c
Arviixzuh <arviixzuh@gmail.com>
2024-06-26 19:37:09 -0400

    style: 🌈 corrección de detalles

--- client/src/renderer/src/components/navbar/Navbartop.tsx
+++ client/src/renderer/src/components/navbar/Navbartop.tsx
@@ -1,48 +1,28 @@
-import React from 'react'
-import { BiMenu, BiCog, BiBell } from 'react-icons/bi'
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  Link,
-  DropdownItem,
-  DropdownTrigger,
-  Dropdown,
-  DropdownMenu,
-  Avatar
-} from '@nextui-org/react'
 import AvatarHelp from '../Avatar/Avatar'
+import { BiBell, BiCog, BiMenu } from 'react-icons/bi'
 
 const Navbartop = () => {
+  const pathList = ['Home', 'Workspaces', 'API Network']
+
   return (
     <nav className="h-[50px] bg-white border-b flex items-center ">
       <div className="w-full h-full px-[18px]">
         <div className="relative flex items-center justify-between w-full h-full  ">
-          <div className=" inset-y-0 left-0 flex items-center w-[50px] flex justify-center items-center text-gray-600 hover:bg-[#eaddff] cursor-pointer">
+          <div className=" inset-y-0 left-0 flex items-center w-[50px] justify-center text-gray-600 hover:bg-[#eaddff] cursor-pointer">
             <BiMenu className=" text-gray-600  text-[20px] w-[25px]  h-[50px]  " />
           </div>
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="hidden sm:block sm:ml-6">
               <div className="flex space-x-4">
-                <a
-                  href="#"
-                  className="text-gray-600 hover:bg-[#eaddff] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Home
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-600 hover:bg-[#eaddff] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Workspaces
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-600 hover:bg-[#eaddff] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  API Network
-                </a>
+                {pathList.map((item, index) => (
+                  <a
+                    key={index}
+                    href="#"
+                    className="text-gray-600 hover:bg-[#eaddff] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    {item}
+                  </a>
+                ))}
               </div>
             </div>
           </div>
