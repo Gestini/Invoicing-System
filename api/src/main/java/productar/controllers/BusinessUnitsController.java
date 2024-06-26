61bcfb24efe1a9a06ca0ed6b90d38204afb46c07
Arviixzuh <arviixzuh@gmail.com>
2024-06-26 19:36:03 -0400

    fix: 🐛 relación entre el usuario y la unidad de negocio

--- api/src/main/java/productar/controllers/BusinessUnitsController.java
+++ api/src/main/java/productar/controllers/BusinessUnitsController.java
@@ -12,8 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import productar.services.BusinessUnitsService;
+
 import productar.models.BusinessUnitsModel;
+import productar.services.BusinessUnitsService;
 
 @RestController
 @RequestMapping("/business-unit")
@@ -23,9 +24,8 @@ public class BusinessUnitsController {
     private BusinessUnitsService businessUnitsService;
 
     @PostMapping("/save")
-    public BusinessUnitsModel saveBusinessUnit(@RequestBody BusinessUnitsModel businessUnit) {
-        System.out.println(businessUnit);
-        return this.businessUnitsService.saveBusinessUnit(businessUnit);
+    public ResponseEntity<String> saveBusinessUnit(@RequestBody BusinessUnitsModel data) {
+        return businessUnitsService.saveBusinessUnit(data);
     }
 
     @GetMapping("/get-all")
@@ -42,5 +42,4 @@ public class BusinessUnitsController {
     public ResponseEntity<String> deleteBusinessUnitById(@PathVariable("id") Long id) {
         return this.businessUnitsService.deleteBusinessUnitById(id);
     }
-
 }
