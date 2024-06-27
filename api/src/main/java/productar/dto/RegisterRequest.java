5eee6d0fabe3978ac6aa362ffe8682b1cc670b11
Arviixzuh <arviixzuh@gmail.com>
2024-06-27 14:32:49 -0400

    feat: 📦 se agregó el email y la contraseña repetida en la request de registro

--- api/src/main/java/productar/dto/RegisterRequest.java
+++ api/src/main/java/productar/dto/RegisterRequest.java
@@ -10,9 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
+    String email;
+    String country;
     String username;
     String password;
-    String firstname;
     String lastname;
-    String country;
+    String firstname;
+    String repeatPassword;
 }
