1b94bfa3c6e68ee49f7df6bb0ea0d7d8db808a2d
Arviixzuh <arviixzuh@gmail.com>
2024-06-27 14:18:15 -0400

    feat: ✨ implementación del middleware que se encarga de validar la sesión del usuario en el lado del cliente

--- client/src/renderer/src/routes/middlewares/ProtectedRouteSession.tsx
+++ client/src/renderer/src/routes/middlewares/ProtectedRouteSession.tsx
@@ -1,17 +1,30 @@
+import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { Navbar } from '@renderer/components/Navbar'
-import { Sidebar } from '@renderer/components/Sidebar'
+import { Navigate } from 'react-router-dom'
+import { setMyUser } from '@renderer/features/userSlice'
+import { useDispatch } from 'react-redux'
+import { reqAuthLoadProfileByToken } from '@renderer/api/requests'
 
 const ProtectedRouteAuth = () => {
-  return (
-    <>
-      <Navbar />
-      <div className='flex'>
-        <Sidebar />
-        <Outlet />
-      </div>
-    </>
-  )
+  const token = localStorage.getItem('token')
+  const dispatch = useDispatch()
+  if (!token) return <Navigate to='/login' />
+
+  React.useEffect(() => {
+    const loadProfile = async () => {
+      try {
+        if (token) {
+          const response = await reqAuthLoadProfileByToken(token)
+          dispatch(setMyUser(response.data))
+        }
+      } catch (error) {
+        localStorage.removeItem('token')
+      }
+    }
+    loadProfile()
+  }, [token])
+
+  return <Outlet />
 }
 
 export default ProtectedRouteAuth
