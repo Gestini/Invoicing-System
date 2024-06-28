9aacd6a7906f83771da24a375ecf44f2d03aeb39
Arviixzuh <arviixzuh@gmail.com>
2024-06-27 20:35:22 -0400

    fix: 🐛 se agregó la constante location al useEffect para que se active cuando el usuario cambie de página

--- client/src/renderer/src/routes/middlewares/ProtectedRouteSession.tsx
+++ client/src/renderer/src/routes/middlewares/ProtectedRouteSession.tsx
@@ -3,9 +3,11 @@ import { Outlet } from 'react-router-dom'
 import { Navigate } from 'react-router-dom'
 import { setMyUser } from '@renderer/features/userSlice'
 import { useDispatch } from 'react-redux'
+import { useLocation } from 'react-router-dom'
 import { reqAuthLoadProfileByToken } from '@renderer/api/requests'
 
 const ProtectedRouteAuth = () => {
+  const localtion = useLocation()
   const token = localStorage.getItem('token')
   const dispatch = useDispatch()
   if (!token) return <Navigate to='/login' />
@@ -19,10 +21,11 @@ const ProtectedRouteAuth = () => {
         }
       } catch (error) {
         localStorage.removeItem('token')
+        window.location.reload()
       }
     }
     loadProfile()
-  }, [token])
+  }, [token, localtion])
 
   return <Outlet />
 }
