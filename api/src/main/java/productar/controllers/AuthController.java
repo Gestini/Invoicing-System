de56fc37d29ee9dab8e5264b0aa142b4e9e7e15d
Arviixzuh <arviixzuh@gmail.com>
2024-06-27 20:36:27 -0400

    refactor: 🛠️ eliminación de importaciones innecesarias

--- api/src/main/java/productar/controllers/AuthController.java
+++ api/src/main/java/productar/controllers/AuthController.java
@@ -1,6 +1,5 @@
 package productar.controllers;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import productar.dto.AuthResponse;
 import productar.dto.LoginRequest;
 import productar.dto.RegisterRequest;
 import productar.models.User;
