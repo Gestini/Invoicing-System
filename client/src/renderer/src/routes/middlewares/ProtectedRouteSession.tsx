ed344d3d34db3fe467430de69fd08c4bde3cf1c6
Arviixzuh <arviixzuh@gmail.com>
2024-06-27 20:47:51 -0400

    fix: 🐛 validación para retornar el contenido de la ruta si es que existe el usuario

--- client/src/renderer/src/routes/middlewares/ProtectedRouteSession.tsx
+++ client/src/renderer/src/routes/middlewares/ProtectedRouteSession.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Navigate } from 'react-router-dom'
 import { setMyUser } from '@renderer/features/userSlice'
-import { useDispatch } from 'react-redux'
 import { useLocation } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { reqAuthLoadProfileByToken } from '@renderer/api/requests'
 
 const ProtectedRouteAuth = () => {
   const location = useLocation()
   const token = localStorage.getItem('token')
   const dispatch = useDispatch()
+
   if (!token) return <Navigate to='/login' />
+  const user = useSelector((state: any) => state.user)
 
   React.useEffect(() => {
     const loadProfile = async () => {
@@ -27,7 +29,7 @@ const ProtectedRouteAuth = () => {
     loadProfile()
   }, [token, location])
 
-  return <Outlet />
+  if (user !== null) return <Outlet />
 }
 
 export default ProtectedRouteAuth
