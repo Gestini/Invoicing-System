54b19948882446f4d51c2d7177e7087963899912
Arviixzuh <arviixzuh@gmail.com>
2024-06-28 23:59:34 -0400

    feat: 📦 implementación del dropdown para exportar las tablas en diferentes formatos

---
+++ client/src/renderer/src/components/AppTable/Exports/ExportTableDropdown.tsx
@@ -0,0 +1,35 @@
+import { useSelector } from 'react-redux'
+import { ChevronDownIcon } from '@renderer/components/Icons'
+import { ExportTableToExcel } from './Excel'
+import { PdfIcon, DocIcon, ExcelIcon } from '@renderer/components/Icons/ExportTableIcons'
+import { Button, Dropdown, DropdownMenu, DropdownItem, DropdownTrigger } from '@nextui-org/react'
+
+export const ExportTableDropdown = () => {
+  const data = useSelector((state: any) => state.table.data)
+
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        <Button endContent={<ChevronDownIcon className='text-small' />} variant='flat'>
+          Exportar
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu variant='faded' aria-label='Dropdown menu with icons'>
+        <DropdownItem
+          key='edit'
+          className='default-text-color'
+          endContent={<ExcelIcon />}
+          onClick={() => ExportTableToExcel(data)}
+        >
+          Documento Excel
+        </DropdownItem>
+        <DropdownItem key='edit2' className='default-text-color' endContent={<DocIcon />}>
+          Documento Word
+        </DropdownItem>
+        <DropdownItem key='edit1' className='default-text-color' endContent={<PdfIcon />}>
+          Documento PDF
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  )
+}
