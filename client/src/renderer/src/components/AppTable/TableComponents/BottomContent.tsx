8eb0f503efe142bcac8999da6bb2392c37dded6c
Arviixzuh <arviixzuh@gmail.com>
2024-06-29 00:01:27 -0400

    style: 🌈 corrección de detalles

--- client/src/renderer/src/components/AppTable/TableComponents/BottomContent.tsx
+++ client/src/renderer/src/components/AppTable/TableComponents/BottomContent.tsx
@@ -24,7 +24,7 @@ export const BottomContent = ({ pages, page, setPage, selectedKeys, filteredItem
         isCompact
         showControls
         showShadow
-        color='primary'
+        color='secondary'
         page={page}
         total={pages}
         onChange={setPage}
