4c392e3bfea82dcc5ea30db56e78f4f8880d9e06
nicoluu <estebaanlunaaa@gmail.com>
2024-07-01 15:53:23 -0300

    stlye: refactor

--- client/src/renderer/src/components/navbar/index.tsx
+++ client/src/renderer/src/components/navbar/index.tsx
@@ -29,12 +29,10 @@ export const Navbar = () => {
   const isDashboardActive = pathList[1] && pathList[1].paths && pathList[1].paths.includes(location.pathname);
 
   return (
-    <nav className='h-[45px] w-full bg-c-bg-color pl-[50px] flex items-center'>
+    <nav className='h-[45px] w-full bg-c-bg-color flex items-center'>
       <div className='w-full h-full px-[18px]'>
         <div className='relative flex items-center justify-between w-full h-full'>
-          <div className='flex items-center justify-center text-c-title cursor-pointer'>
-            My Space
-          </div>
+          <div className='flex items-center justify-center text-c-title cursor-pointer w-16'></div>
           <div className='flex items-center justify-between'>
             <div className='hidden sm:block'>
               <div className='flex space-x-4'>
@@ -52,9 +50,9 @@ export const Navbar = () => {
             </div>
           </div>
           <div className='inset-y-0 right-0 flex items-center'>
-            <div className='settings w-[50px] flex justify-center h-[50px] hover:bg-[#eaddff] cursor-pointer'>
+            {/* <div className='settings w-[50px] flex justify-center h-[50px] hover:bg-[#eaddff] cursor-pointer'>
               <BiCog className='h-full text-[20px] text-gray-600' />
-            </div>
+            </div> */}
             <div className='settings w-[50px] flex justify-center h-[50px] hover:bg-[#eaddff] cursor-pointer'>
               <BiBell className='h-full text-[20px] text-gray-600' />
             </div>
