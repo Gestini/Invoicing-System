66e35460a6a63f9ae7b29be1c1d45dd31bfd8ebf
Arviixzuh <arviixzuh@gmail.com>
2024-07-01 20:32:09 -0400

    #

--- client/src/renderer/src/store/index.ts
+++ client/src/renderer/src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { manageUserSlice } from '@renderer/features/userSlice'
 import { manageTableSlice } from '@renderer/features/tableSlice'
+import { manageInvoicingSlice } from '@renderer/features/invoicingSlice'
 
 const store = configureStore({
   reducer: {
     user: manageUserSlice.reducer,
     table: manageTableSlice.reducer,
+    invoicing: manageInvoicingSlice.reducer,
   },
 })
 
