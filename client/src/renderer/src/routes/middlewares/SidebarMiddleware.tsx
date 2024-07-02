6cd8e33197091cd9d8f4c523cc9bc1cd636d5ded
Arviixzuh <arviixzuh@gmail.com>
2024-07-02 01:52:35 -0400

    fix: 🐛 error en el middleware al cagar las unidades

--- client/src/renderer/src/routes/middlewares/SidebarMiddleware.tsx
+++ client/src/renderer/src/routes/middlewares/SidebarMiddleware.tsx
@@ -1,12 +1,11 @@
-import { reqGetUnitById } from '@renderer/api/requests'
-import { capitalize } from '@renderer/components/AppTable/TableComponents/utils'
-import { Sidebar } from '@renderer/components/Sidebar'
 import React from 'react'
-import { Outlet, useLocation } from 'react-router-dom'
-
+import { Sidebar } from '@renderer/components/Sidebar'
 import { setUnit } from '@renderer/features/currentUnitSlice'
-import { useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import { capitalize } from '@renderer/components/AppTable/TableComponents/utils'
+import { reqGetUnitById } from '@renderer/api/requests'
+import { Outlet, useLocation } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import './bodymain.scss'
 
 export const SidebarMiddleware = () => {
@@ -14,13 +13,13 @@ export const SidebarMiddleware = () => {
   const name = location.pathname.replace('/', '')
   const params = useParams()
   const dispatch = useDispatch()
+  const unit = useSelector((state: any) => state.currentUnit)
 
   React.useEffect(() => {
     const loadUserCompanies = async () => {
       try {
         const response = await reqGetUnitById(params.id)
         dispatch(setUnit(response.data))
-        console.log(response.data)
       } catch (error) {
         console.error('Error fetching business units:', error)
       }
@@ -32,10 +31,12 @@ export const SidebarMiddleware = () => {
     <>
       <div className='ml-[55px] p-[20px] bodymain '>
         <Sidebar />
-        <div className='w-full h-full flex flex-col gap-4'>
-          <h5 className='text-4xl font-semibold text-c-primary'>{capitalize(name)}</h5>
-          <Outlet />
-        </div>
+        {unit !== null && (
+          <div className='w-full h-full flex flex-col gap-4'>
+            <h5 className='text-4xl font-semibold text-c-primary'>{capitalize(name)}</h5>
+            <Outlet />
+          </div>
+        )}
       </div>
     </>
   )
