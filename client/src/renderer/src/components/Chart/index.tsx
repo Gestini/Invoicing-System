e41a80b04ed5975105cf5a67c5188cc3403c0f5a
Arviixzuh <arviixzuh@gmail.com>
2024-07-04 19:05:20 -0400

    refactor: 🛠️ modularización de componentes

--- client/src/renderer/src/components/Chart/index.tsx
+++ client/src/renderer/src/components/Chart/index.tsx
@@ -1,61 +1,70 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { PieChart } from 'react-minimal-pie-chart';
-import { ColorType, createChart } from 'lightweight-charts';
-import { Card, CardBody, CardHeader } from '@nextui-org/react';
+import React from 'react'
+import { PieChart } from 'react-minimal-pie-chart'
+import { useSelector } from 'react-redux'
+import { ColorType, createChart } from 'lightweight-charts'
+import { Card, CardBody, CardHeader } from '@nextui-org/react'
 
 // Función para convertir un color hex a HSL
 const hexToHSL = (hex: string): [number, number, number] => {
-  hex = hex.replace('#', '');
-  const r = parseInt(hex.slice(0, 2), 16) / 255;
-  const g = parseInt(hex.slice(2, 4), 16) / 255;
-  const b = parseInt(hex.slice(4, 6), 16) / 255;
+  hex = hex.replace('#', '')
+  const r = parseInt(hex.slice(0, 2), 16) / 255
+  const g = parseInt(hex.slice(2, 4), 16) / 255
+  const b = parseInt(hex.slice(4, 6), 16) / 255
 
-  const max = Math.max(r, g, b);
-  const min = Math.min(r, g, b);
-  let h, s, l = (max + min) / 2;
+  const max = Math.max(r, g, b)
+  const min = Math.min(r, g, b)
+  let h,
+    s,
+    l = (max + min) / 2
 
   if (max === min) {
-    h = s = 0; // achromatic
+    h = s = 0 // achromatic
   } else {
-    const d = max - min;
-    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    const d = max - min
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min)
     switch (max) {
-      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-      case g: h = (b - r) / d + 2; break;
-      case b: h = (r - g) / d + 4; break;
-      default: break;
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0)
+        break
+      case g:
+        h = (b - r) / d + 2
+        break
+      case b:
+        h = (r - g) / d + 4
+        break
+      default:
+        break
     }
-    h /= 6;
+    h /= 6
   }
 
-  return [h * 360, s * 100, l * 100];
-};
+  return [h * 360, s * 100, l * 100]
+}
 
 // Función para generar variantes del color principal
 const generateColorVariants = (mainColor: string, count: number): string[] => {
-  const [h, s, l] = hexToHSL(mainColor);
-  const variants: string[] = [];
+  const [h, s, l] = hexToHSL(mainColor)
+  const variants: string[] = []
 
   for (let i = 0; i < count; i++) {
-    const newL = (l + (i * 3.5)) % 100; // ajustar el nivel de luz
-    variants.push(`hsl(${h}, ${s}%, ${newL}%)`);
+    const newL = (l + i * 3.5) % 100 // ajustar el nivel de luz
+    variants.push(`hsl(${h}, ${s}%, ${newL}%)`)
   }
 
-  console.log(variants);
-  return variants;
-};
+  return variants
+}
 
 const ChartComponent = ({ data, colors }) => {
-  const chartContainerRef = useRef<HTMLDivElement | null>(null);
+  const chartContainerRef = React.useRef<HTMLDivElement | null>(null)
 
-  useEffect(() => {
-    if (!chartContainerRef.current) return;
+  React.useEffect(() => {
+    if (!chartContainerRef.current) return
 
     const handleResize = () => {
       if (chartContainerRef.current) {
-        chart.applyOptions({ width: chartContainerRef.current.clientWidth });
+        chart.applyOptions({ width: chartContainerRef.current.clientWidth })
       }
-    };
+    }
 
     const chart = createChart(chartContainerRef.current, {
       layout: {
@@ -72,50 +81,40 @@ const ChartComponent = ({ data, colors }) => {
           color: 'transparent',
         },
       },
-    });
-    chart.timeScale().fitContent();
+    })
+    chart.timeScale().fitContent()
 
     const newSeries = chart.addAreaSeries({
       lineColor: colors.lineColor,
       topColor: colors.areaTopColor,
       bottomColor: colors.areaBottomColor,
-    });
-    newSeries.setData(data);
+    })
+    newSeries.setData(data)
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener('resize', handleResize)
 
     return () => {
-      window.removeEventListener('resize', handleResize);
-      chart.remove();
-    };
-  }, [data, colors]);
+      window.removeEventListener('resize', handleResize)
+      chart.remove()
+    }
+  }, [data, colors])
 
-  return <div ref={chartContainerRef} />;
-};
+  return <div ref={chartContainerRef} />
+}
 
 export const GraphView = () => {
-  const [mainColor, setMainColor] = useState('');
+  const [mainColor, setMainColor] = React.useState('')
+  const [parsedMainColor, setParsedMainColor] = React.useState('')
+  const currentTheme = useSelector((state: any) => state.currentTheme)
 
-  useEffect(() => {
-    const style = getComputedStyle(document.body);
-    const computedMainColor = style.getPropertyValue('--c-primary').trim();
-    setMainColor(computedMainColor);
-
-    const observer = new MutationObserver((mutationsList) => {
-      for (let mutation of mutationsList) {
-        if (mutation.type === 'attributes' && mutation.attributeName === 'id') {
-          const newMainColor = getComputedStyle(document.body).getPropertyValue('--c-primary').trim();
-          setMainColor(newMainColor);
-        }
-      }
-    });
-
-    observer.observe(document.body, { attributes: true });
-
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
+  React.useEffect(() => {
+    const style = getComputedStyle(document.body)
+    const computedMainColor = style.getPropertyValue('--c-primary').trim()
+    setMainColor(computedMainColor)
+    setParsedMainColor(
+      `rgba(${parseInt(computedMainColor?.slice(1, 3), 16)}, ${parseInt(computedMainColor?.slice(3, 5), 16)}, ${parseInt(computedMainColor?.slice(5, 7), 16)}, 0.1)`,
+    )
+  }, [currentTheme])
 
   const graphList = [
     {
@@ -125,7 +124,7 @@ export const GraphView = () => {
         lineColor: mainColor,
         textColor: '#71717a',
         areaTopColor: mainColor,
-        areaBottomColor: `rgba(${parseInt(mainColor.slice(1, 3), 16)}, ${parseInt(mainColor.slice(3, 5), 16)}, ${parseInt(mainColor.slice(5, 7), 16)}, 0.1)`,
+        areaBottomColor: parsedMainColor,
       },
       initialData: [
         { time: '2018-12-22', value: 1 },
@@ -147,7 +146,7 @@ export const GraphView = () => {
         lineColor: mainColor,
         textColor: '#71717a',
         areaTopColor: mainColor,
-        areaBottomColor: `rgba(${parseInt(mainColor.slice(1, 3), 16)}, ${parseInt(mainColor.slice(3, 5), 16)}, ${parseInt(mainColor.slice(5, 7), 16)}, 0.1)`,
+        areaBottomColor: parsedMainColor,
       },
       initialData: [
         { time: '2018-12-22', value: 1 },
@@ -162,13 +161,13 @@ export const GraphView = () => {
         { time: '2018-12-31', value: 8 },
       ],
     },
-  ];
+  ]
 
   const pieChartData = generateColorVariants(mainColor, 7).map((color, index) => ({
     title: `Variant ${index + 1}`,
     value: 10, // o cualquier otro valor que necesites
     color,
-  }));
+  }))
 
   return (
     <div className='flex gap-4'>
@@ -177,10 +176,7 @@ export const GraphView = () => {
           <h3 className='font-medium'>Stock</h3>
         </CardHeader>
         <CardBody className='flex items-center'>
-          <PieChart
-            style={{ maxWidth: '220px' }}
-            data={pieChartData}
-          />
+          <PieChart style={{ maxWidth: '220px' }} data={pieChartData} />
         </CardBody>
       </Card>
       {graphList.map((item, index) => (
@@ -194,5 +190,5 @@ export const GraphView = () => {
         </Card>
       ))}
     </div>
-  );
-};
+  )
+}
