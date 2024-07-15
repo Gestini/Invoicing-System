e4e197baca0315be4a1abc04f3f0d42760c0f238
Arviixzuh <arviixzuh@gmail.com>
2024-07-15 00:50:30 -0400

    feat: 📦 se implementó el decorador @OnDelete en la relación entre el depósito y el producto

--- api/src/main/java/productar/models/ProductModel.java
+++ api/src/main/java/productar/models/ProductModel.java
@@ -2,6 +2,9 @@ package productar.models;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -99,6 +102,7 @@ public class ProductModel {
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "deposit_unit_id", nullable = true)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private DepositUnitsModel depositUnit;
 
     @Column(name = "price_policy")
