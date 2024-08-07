3245b8be4825185a686b566cc541fd87d4eaf22e
arviixzuh <victorgabriel725456@gmail.com>
2024-08-07 15:01:09 -0400

    feat: 📦 implementación de una función para obtener a los empleados de un que tienen un rol específico

--- api/src/main/java/productar/repositories/RoleUsersRepository.java
+++ api/src/main/java/productar/repositories/RoleUsersRepository.java
@@ -1,5 +1,6 @@
 package productar.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,11 +15,15 @@ import productar.models.RoleUsersModel;
 @Repository
 public interface RoleUsersRepository extends JpaRepository<RoleUsersModel, Long> {
 
-    @Query("SELECT ru FROM RoleUsersModel ru WHERE ru.role.id = :roleId AND ru.user.id = :userId")
+    @Query("SELECT ru FROM RoleUsersModel ru WHERE ru.role.id = :roleId AND ru.employee.id = :userId")
     Optional<RoleUsersModel> userHasRole(@Param("roleId") Long roleId, @Param("userId") Long userId);
 
     @Modifying
     @Transactional
-    @Query("DELETE FROM RoleUsersModel ru WHERE ru.role.id = :roleId AND ru.user.id = :userId")
+    @Query("DELETE FROM RoleUsersModel ru WHERE ru.role.id = :roleId AND ru.employee.id = :userId")
     void removeRole(@Param("roleId") Long roleId, @Param("userId") Long userId);
+
+    @Query("SELECT ru FROM RoleUsersModel ru WHERE ru.role.id = :roleId")
+    List<RoleUsersModel> findEmployeesByRole(@Param("roleId") Long roleId);
+
 }
