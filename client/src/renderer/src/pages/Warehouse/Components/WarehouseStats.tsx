da1b96e79d1235ec2258c9e448a43aead6671936
Arviixzuh <arviixzuh@gmail.com>
2024-08-16 00:37:06 -0400

    style: 🌈 corrección de detalles

--- client/src/renderer/src/pages/Warehouse/Components/WarehouseStats.tsx
+++ client/src/renderer/src/pages/Warehouse/Components/WarehouseStats.tsx
@@ -40,7 +40,7 @@ export const WarehouseStats = () => {
   if (currentWarehouseId == '' || warehouse.data.length === 0) return
 
   return (
-    <div className='flex flex-wrap gap-[53px]'>
+    <div className='flex flex-wrap gap-[33px] mb-4'>
       {datita.map((ele, ind) => (
         <div key={ind}>
           <p className='flex gap-1 items-center text-c-gray text-[16px]'>
