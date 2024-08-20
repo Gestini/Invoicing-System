8203cc7d51725a1167f99c7ef2dbaca74e5fb2ab
Arviixzuh <arviixzuh@gmail.com>
2024-08-19 21:35:53 -0400

    feat: 📖 visualizar el estado y el email de un empleado en la tabla

--- client/src/renderer/src/pages/EmployeeTable/data.ts
+++ client/src/renderer/src/pages/EmployeeTable/data.ts
@@ -4,16 +4,19 @@ export const columnsData = {
     { name: 'PIN', uid: 'pin' },
     { name: 'NOMBRE', uid: 'name' },
     { name: 'APELLIDO', uid: 'lastname' },
+    { name: 'EMAIL', uid: 'email' },
+    { name: 'STATUS', uid: 'status' },
     { name: 'ACCIÓN', uid: 'actions' },
   ],
   statusOptions: [
-    { name: 'Completado', uid: 'COMPLETED' },
     { name: 'Pendiente', uid: 'PENDING' },
-    { name: 'Cancelado', uid: 'CANCELLED' },
+    { name: 'Activo', uid: 'ACTIVE' },
   ],
   InitialVisibleColumns: [
     'name',
     'lastname',
+    'email',
+    'status',
     'id',
     'actions',
   ],
@@ -24,12 +27,17 @@ export const modalInputs = {
     {
       type: 'text',
       name: 'name',
-      label: 'Nombre del empleado',
+      label: 'Nombre',
     },
     {
       type: 'text',
       name: 'lastname',
       label: 'Apellido',
+    },
+    {
+      type: 'email',
+      name: 'email',
+      label: 'Email',
     },
     {
       type: 'text',
