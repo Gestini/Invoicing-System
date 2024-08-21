3a7c9fd5481fedf86376ee28da53a29aa5742e67
Arviixzuh <arviixzuh@gmail.com>
2024-08-21 00:19:31 -0400

    fix: 🐛 configuración de Axios para establecer el token del usuario en los headers de autenticación sin importar que el mismo cambie

--- client/src/renderer/src/api/axios.ts
+++ client/src/renderer/src/api/axios.ts
@@ -6,7 +6,23 @@ export const authApi = axios.create({
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_SERVER_API,
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem('token')}`,
-  },
+})
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+
+api.interceptors.response.use((response) => {
+  return response
+}, (error) => {
+  if (error.response && error.response.status === 401) {
+    console.error('Token expired or invalid')
+  }
+  return Promise.reject(error)
 })
