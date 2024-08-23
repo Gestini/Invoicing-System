e992d1262e4251ed866533d03d06f41a6dbbe70b
Arviixzuh <arviixzuh@gmail.com>
2024-08-23 01:33:32 -0400

    fix: 🐛 nueva validación que solo verifica si el empleado se encuentra en la unidad sin importar el rol

--- api/src/main/java/productar/repositories/RoleUsersRepository.java
+++ api/src/main/java/productar/repositories/RoleUsersRepository.java
@@ -18,13 +18,19 @@ public interface RoleUsersRepository extends JpaRepository<RoleUsersModel, Long>
     @Query("SELECT ru FROM RoleUsersModel ru WHERE ru.role.id = :roleId AND ru.employee.id = :userId")
     Optional<RoleUsersModel> userHasRole(@Param("roleId") Long roleId, @Param("userId") Long userId);
 
-    @Query("SELECT EXISTS (SELECT 1 FROM BusinessUnitsModel b WHERE b.id = :unitId AND b.owner.id = :userId) "
-            + "OR EXISTS (SELECT 1 FROM RoleUsersModel ru "
+    @Query("SELECT EXISTS ("
+            + "SELECT 1 FROM BusinessUnitsModel b "
+            + "WHERE b.id = :unitId AND b.owner.id = :userId) "
+            + "OR ("
+            + "EXISTS (SELECT 1 FROM EmployeeModel e "
+            + "WHERE e.user.id = :userId AND e.businessUnit.id = :unitId) "
+            + "AND (:permissionName = 'none' OR EXISTS ("
+            + "SELECT 1 FROM RoleUsersModel ru "
             + "JOIN RoleModel r ON ru.role.id = r.id "
             + "JOIN RolePermissionsModel rp ON r.id = rp.role.id "
             + "WHERE ru.employee.user.id = :userId "
             + "AND r.businessUnit.id = :unitId "
-            + "AND (rp.name = :permissionName OR rp.name = '*'))")
+            + "AND (rp.name = :permissionName OR rp.name = '*'))))")
     boolean hasPermissions(@Param("userId") Long userId,
             @Param("unitId") Long unitId,
             @Param("permissionName") String permissionName);
