57ccfee956aac6a9051577fe973d4a5bf20f42fa
Arviixzuh <arviixzuh@gmail.com>
2024-09-06 17:40:07 -0400

    feat: 📖 se implementó un useEffect que carga el estado del sidebar guardado en el localstorage

--- client/src/renderer/src/routes/middlewares/NavbarAndSidebarMiddleware.tsx
+++ client/src/renderer/src/routes/middlewares/NavbarAndSidebarMiddleware.tsx
@@ -1,11 +1,22 @@
-import { Navbar } from '@renderer/components/Navbar'
-import { Sidebar } from '@renderer/components/Sidebar'
+import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Sidebar } from '@renderer/components/Sidebar'
+import { useDispatch } from 'react-redux'
+import { setSidebarState } from '@renderer/features/sidebarSlice'
 
 export const NavbarAndSidebarMiddleware = () => {
+  const dispatch = useDispatch()
+
+  React.useEffect(() => {
+    const sidebarLocalState = localStorage.getItem('sidebarState')
+    if (!sidebarLocalState) return
+    if (sidebarLocalState === 'active') {
+      dispatch(setSidebarState(true))
+    }
+  }, [])
+
   return (
     <div className='flex'>
-      <Navbar />
       <Sidebar />
       <Outlet />
     </div>
