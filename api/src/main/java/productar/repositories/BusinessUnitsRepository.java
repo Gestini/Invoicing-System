3b4b796c4fdc0ebb24623dc1899ef0d803fe1a7b
Arviixzuh <arviixzuh@gmail.com>
2024-09-11 12:38:13 -0400

    feat: 📖 consulta que trae información de una unidad que tenga activo el e-commerce

--- api/src/main/java/productar/repositories/BusinessUnitsRepository.java
+++ api/src/main/java/productar/repositories/BusinessUnitsRepository.java
@@ -3,13 +3,19 @@ package productar.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import productar.models.BusinessUnitsModel;
+import productar.models.BusinessUnitModel;
 
 @Repository
-public interface BusinessUnitsRepository extends JpaRepository<BusinessUnitsModel, Long> {
-    List<BusinessUnitsModel> findByOwnerUsername(String username);
-    List<BusinessUnitsModel> findByEcommerceTrue();
-}
+public interface BusinessUnitsRepository extends JpaRepository<BusinessUnitModel, Long> {
+    List<BusinessUnitModel> findByOwnerUsername(String username);
 
+    @Query("SELECT unit FROM BusinessUnitModel unit WHERE unit.ecommerce = true")
+    List<BusinessUnitModel> findAllUnitsWithEcommerce();
+
+    @Query("SELECT unit FROM BusinessUnitModel unit WHERE unit.id = :unitId AND unit.ecommerce = true")
+    BusinessUnitModel findByUnitIdAndEcommerce(@PathVariable("unitId") Long unitId);
+}
