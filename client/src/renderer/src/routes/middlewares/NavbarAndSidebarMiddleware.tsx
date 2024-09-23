ae81e340df4eeaa071b610ffa69a2269a3ffaf3e
Arviixzuh <arviixzuh@gmail.com>
2024-09-23 19:29:56 -0400

    #

--- client/src/renderer/src/routes/middlewares/NavbarAndSidebarMiddleware.tsx
+++ client/src/renderer/src/routes/middlewares/NavbarAndSidebarMiddleware.tsx
@@ -16,7 +16,7 @@ export const NavbarAndSidebarMiddleware = () => {
   }, [])
 
   return (
-    <div className='flex overflow-hidden max-h-screen'>
+    <div className='flex overflow-auto max-h-screen'>
       <Sidebar />
       <Outlet />
     </div>
