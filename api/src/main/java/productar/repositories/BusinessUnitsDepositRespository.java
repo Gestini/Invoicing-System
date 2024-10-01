c81925add61013529d7d20128540d5fc35196015
Arviixzuh <arviixzuh@gmail.com>
2024-10-01 08:37:25 -0400

    feat: ✨ implementación de método para desvincular un depósito de una unidad

--- api/src/main/java/productar/repositories/BusinessUnitsDepositRespository.java
+++ api/src/main/java/productar/repositories/BusinessUnitsDepositRespository.java
@@ -3,9 +3,11 @@ package productar.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import jakarta.transaction.Transactional;
 import productar.models.BusinessUnitDepositModel;
 
 public interface BusinessUnitsDepositRespository extends JpaRepository<BusinessUnitDepositModel, Long> {
@@ -14,4 +16,9 @@ public interface BusinessUnitsDepositRespository extends JpaRepository<BusinessU
 
     @Query("SELECT COUNT(bd) > 0 FROM BusinessUnitDepositModel bd WHERE bd.businessUnit.id = :unitId AND bd.deposit.id = :depositId")
     boolean isAssigned(@PathVariable("unitId") Long unitId, @PathVariable("depositId") Long depositId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM BusinessUnitDepositModel bd WHERE bd.businessUnit.id = :unitId AND bd.deposit.id = :depositId")
+    int unlinkDepositFromUnit(@PathVariable("unitId") Long unitId, @PathVariable("depositId") Long depositId);
 }
