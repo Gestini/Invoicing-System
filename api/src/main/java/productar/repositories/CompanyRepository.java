e9d9ce90a2825394302237f02d2e99b60fa91aa2
Arviixzuh <arviixzuh@gmail.com>
2024-10-01 08:38:12 -0400

    #

---
+++ api/src/main/java/productar/repositories/CompanyRepository.java
@@ -0,0 +1,17 @@
+package productar.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import productar.models.CompanyModel;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<CompanyModel, Long> {
+
+    @Query("SELECT company FROM CompanyModel company WHERE company.owner.id = :ownerId")
+    List<CompanyModel> findCompanyByOwnerId(@PathVariable("ownerId") Long ownerId);
+}
