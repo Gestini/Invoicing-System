4030fa2a6fda5013c08ac00a2f766fb4549c7a4d
Arviixzuh <arviixzuh@gmail.com>
2024-10-05 00:12:10 -0400

    chore: 📖 organización del layout principal de la plataforma

---
+++ client/src/renderer/src/layouts/PageLayout.tsx
@@ -0,0 +1,36 @@
+import { Navbar } from '@renderer/components/Navbar'
+import { RootState } from '@renderer/store'
+import { capitalize } from '@renderer/components/AppTable/TableComponents/utils'
+import { useSelector } from 'react-redux'
+
+export const PageLayout = ({ icon, title, section, children, routesLength }) => {
+  const unit = useSelector((state: RootState) => state.currentUnit)
+
+  return (
+    <div className='w-full flex-col flex pl-[100px] md:pl-0'>
+      <Navbar />
+      {unit.id && (
+        <div className='flex flex-col gap-4 pr-3 pl-3 pb-3'>
+          {routesLength === 1 ? (
+            <div>
+              <h6 className='font-semibold flex items-center text-[#4f4d4d]'>
+                {icon}
+                <span className='ml-1'>{section}</span>
+              </h6>
+            </div>
+          ) : (
+            <div>
+              <h6 className='font-semibold flex items-center text-[#4f4d4d]'>
+                {icon}
+                <span className='ml-1'>{section} /</span>
+                <span className='ml-1 text-c-title'>{capitalize(title)}</span>
+              </h6>
+              <h5 className='text-[24px] font-semibold text-c-title'>Gestión de {title}</h5>
+            </div>
+          )}
+          <div>{children}</div>
+        </div>
+      )}
+    </div>
+  )
+}
