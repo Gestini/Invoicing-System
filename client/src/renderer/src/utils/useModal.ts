946acb1a19c2f70674197d66b75f443307e557bd
Arviixzuh <arviixzuh@gmail.com>
2024-10-07 21:14:11 -0400

    feat: ✨ implementación de nuevos tipos de modales

--- client/src/renderer/src/utils/useModal.ts
+++ client/src/renderer/src/utils/useModal.ts
@@ -6,12 +6,18 @@ export const modalTypes = Object.freeze({
   settingsModal: "SETTINGS_MODAL",
   createUnitModal: "CREATE_UNIT_MODAL",
   editDocumentMoal: "EDIT_DOCUMENT_MODAL",
+  editProductModal: "EDIT_PRODUCT_MODAL",
+  editSupplierModal: "EDIT_SUPLIER_MODAL",
   addNewAccountModal: "ADD_NEW_ACCOUNT_MODAL",
   editWarehouseModal: "EDIT_WAREHOUSE_MODAL",
+  editItemTableModal: "EDIT_ITEM_TABLE_MODAL",
+  addItemToTableModal: "ADD_ITEM_TO_TABLE_MODAL",
+  createSupplierModal: "CREATE_SUPLIER_MODAL",
   createDocumentModal: "CREATE_DOCUMENT_MODAL",
   createWarehouseModal: "CREATE_WAREHOUSE_MODAL",
   companySettingsModal: "COMPANY_SETTINGS_MODAL",
   assingDepositToUnitModal: "ASSING_DEPOSIT_TO_UNIT_MODAL",
+  moveProductToInventoryModal: "MOVE_PRODUCT_TO_INVENTORY_MODAL",
 } as const)
 
 type ModalType = (typeof modalTypes)[keyof typeof modalTypes]
