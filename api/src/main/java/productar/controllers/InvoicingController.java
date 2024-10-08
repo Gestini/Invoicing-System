12f290508b4524ae72a509a3b505ec50545870ca
Arviixzuh <arviixzuh@gmail.com>
2024-10-07 21:34:58 -0400

    feat: 🌱 sistema de facturación con productos del inventario de la sucursal

--- api/src/main/java/productar/controllers/InvoicingController.java
+++ api/src/main/java/productar/controllers/InvoicingController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,11 +26,7 @@ public class InvoicingController {
 
     @PostMapping("/save")
     public ResponseEntity<?> saveInvoice(@RequestBody InvoicingRequestDTO data) {
-        try {
-            return invoicingService.saveInvoicing(data);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocurrió un error: " + e.getMessage());
-        }
+        return invoicingService.saveInvoicing(data);
     }
 
     @GetMapping("/get-all")
