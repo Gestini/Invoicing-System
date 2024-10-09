96502416a434bc6217048b604245e77692d519aa
Arviixzuh <arviixzuh@gmail.com>
2024-10-09 10:18:07 -0400

    feat: 📦 implementación de reducers para manejar las secciones que se pueden ver en el sidebar según el sistema de roles

--- client/src/renderer/src/features/sidebarSlice.ts
+++ client/src/renderer/src/features/sidebarSlice.ts
@@ -1,19 +1,35 @@
 import { createSlice } from '@reduxjs/toolkit'
-
-export interface sidebarStateType {
-  isActive: boolean
-}
+import { permissions } from '@renderer/pages/Roles/Permissions'
 
 export const manageSidebarSlice = createSlice({
   name: 'sidebar',
   initialState: {
-    isActive: false,
-  } as sidebarStateType,
+    views: {},
+    isActive: null,
+    showAllViews: false,
+  },
   reducers: {
     setSidebarState: (state, action) => {
       state.isActive = action.payload
     },
+    setViews: (state, action) => {
+      state.views = action.payload
+      state.showAllViews = action.payload[permissions.admin.permission]
+    },
+    handleShowAllViews: (state, _) => {
+      state.showAllViews = !state.showAllViews
+    },
+    changePermissionView: (state, action) => {
+      const { permission } = action.payload
+
+      if (!(permission in state.views)) return
+      state.views[permission] = !state.views[permission]
+
+      if (permission === permissions.admin.permission) {
+        state.showAllViews = !state.showAllViews
+      }
+    }
   },
 })
 
-export const { setSidebarState } = manageSidebarSlice.actions
+export const { setSidebarState, setViews, changePermissionView, handleShowAllViews } = manageSidebarSlice.actions
