fb89bd0982d86bf24329204e94115c47b3771bdb
Arviixzuh <arviixzuh@gmail.com>
2024-10-26 18:54:44 -0400

    feat(permission): 📖 incluir permisos del plan actual de la empresa en la verificación

--- api/src/main/java/gestini/modules/role/repositories/RoleUsersRepository.java
+++ api/src/main/java/gestini/modules/role/repositories/RoleUsersRepository.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
 
 import jakarta.transaction.Transactional;
 import gestini.modules.role.models.RoleUsersModel;
+import gestini.utils.Permission;
 
 @Repository
 public interface RoleUsersRepository extends JpaRepository<RoleUsersModel, Long> {
@@ -18,22 +19,45 @@ public interface RoleUsersRepository extends JpaRepository<RoleUsersModel, Long>
     @Query("SELECT ru FROM RoleUsersModel ru WHERE ru.role.id = :roleId AND ru.employee.id = :userId")
     Optional<RoleUsersModel> userHasRole(@Param("roleId") Long roleId, @Param("userId") Long userId);
 
-    @Query("SELECT EXISTS ("
-            + "SELECT 1 FROM BusinessUnitModel b "
-            + "WHERE b.id = :unitId AND b.company.owner.id = :userId) "
-            + "OR ("
-            + "EXISTS (SELECT 1 FROM EmployeeModel e "
-            + "WHERE e.user.id = :userId AND e.businessUnit.id = :unitId AND e.status = ACTIVE) "
-            + "AND (:permissionName = 'none' OR EXISTS ("
-            + "SELECT 1 FROM RoleUsersModel ru "
-            + "JOIN RoleModel r ON ru.role.id = r.id "
-            + "JOIN RolePermissionsModel rp ON r.id = rp.role.id "
-            + "WHERE ru.employee.user.id = :userId "
-            + "AND r.businessUnit.id = :unitId "
-            + "AND (rp.name = :permissionName OR rp.name = '*'))))")
+    @Query("SELECT " +
+            "    :permissionName = 'DEFAULT_ACCESS' OR EXISTS ( " +
+            "        SELECT 1 " +
+            "        FROM BusinessUnitModel b " +
+            "        JOIN b.company cm " +
+            "        JOIN cm.plan cpm " +
+            "        JOIN PlanPermissionsModel ppm ON cpm.plan.id = ppm.plan.id " +
+            "        WHERE b.id = :unitId " +
+            "        AND cm.owner.id = :userId " +
+            "        AND (ppm.name = :permissionName OR ppm.name = 'ADMIN') " +
+            "    ) " +
+            "OR ( " +
+            "    EXISTS ( " +
+            "        SELECT 1 " +
+            "        FROM EmployeeModel e " +
+            "        WHERE e.user.id = :userId " +
+            "        AND e.businessUnit.id = :unitId " +
+            "        AND e.status = 'ACTIVE' " +
+            "    ) " +
+            "    AND ( " +
+            "        :permissionName = 'DEFAULT_ACCESS' " +
+            "        OR EXISTS ( " +
+            "            SELECT 1 " +
+            "            FROM RoleUsersModel ru " +
+            "            JOIN ru.role r " +
+            "            JOIN RolePermissionsModel rp ON r.id = rp.role.id " +
+            "            JOIN CompanyModel cm ON cm.id = r.businessUnit.company.id " +
+            "            JOIN cm.plan cpm " +
+            "            JOIN PlanPermissionsModel ppm ON cpm.plan.id = ppm.plan.id " +
+            "            WHERE ru.employee.user.id = :userId " +
+            "            AND r.businessUnit.id = :unitId " +
+            "            AND (rp.name = :permissionName OR rp.name = 'ADMIN') " +
+            "            AND (ppm.name = :permissionName OR ppm.name = 'ADMIN') " +
+            "        ) " +
+            "    ) " +
+            ")")
     boolean hasPermissions(@Param("userId") Long userId,
             @Param("unitId") Long unitId,
-            @Param("permissionName") String permissionName);
+            @Param("permissionName") Permission permission);
 
     @Modifying
     @Transactional
