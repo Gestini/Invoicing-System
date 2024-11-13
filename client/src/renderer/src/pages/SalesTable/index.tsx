d00c282f8d813abf53f8a4c5a4199d530f0ad7e7
Arviixzuh <arviixzuh@gmail.com>
2024-11-12 22:52:25 -0400

    feat: 📊 implementación de tabla para ver los productos de una factura

--- client/src/renderer/src/pages/SalesTable/index.tsx
+++ client/src/renderer/src/pages/SalesTable/index.tsx
@@ -2,18 +2,17 @@ import React from 'react'
 import toast from 'react-hot-toast'
 import { AppTable } from '@renderer/components/AppTable'
 import { useParams } from 'react-router-dom'
-import { RootState } from '@renderer/store'
-import { AddItemModal } from '@renderer/components/AppTable/Modals/AddItem'
-import { EditItemModal } from '@renderer/components/AppTable/Modals/EditItem'
-import { useDispatch, useSelector } from 'react-redux'
-import { columnsData, modalInputs } from './data'
-import { reqGetAllInvoicesByUnit, reqDeleteInvoice } from '@renderer/api/requests'
-import { addItem, editItem, deleteItem, setTableData } from '@renderer/features/tableSlice'
+import { columnsData } from './data'
+import { useDispatch } from 'react-redux'
+import { MdShoppingCart } from 'react-icons/md'
+import { ViewProductsModal } from './ViewProductsModal'
+import { DropdownItemInteface } from '@renderer/components/AppTable/Interfaces/ActionDropdown'
+import { reqDeleteInvoice, reqGetAllInvoicesByUnit } from '@renderer/api/requests'
+import { deleteItem, setCurrentItemId, setTableData } from '@renderer/features/tableSlice'
 
 export const SalesTable = () => {
   const dispatch = useDispatch()
   const params = useParams()
-  const table = useSelector((state: RootState) => state.unit.table)
 
   React.useEffect(() => {
     const loadData = async () => {
@@ -27,49 +26,46 @@ export const SalesTable = () => {
     delete: async (id: any) => {
       try {
         dispatch(deleteItem(id))
-        toast.success('Venta eliminada correctamente')
         reqDeleteInvoice(id)
       } catch (error: any) {
         toast.error(error.response.data.message)
       }
     },
-    create: async (data: any) => {
-      try {
-        dispatch(addItem({ ...data, id: table.data.length }))
-        toast.success('Venta guardada correctamente')
-      } catch (error: any) {
-        toast.error(error.response.data.message)
-      }
-    },
-    edit: async (data: any, currentUserEdit: any) => {
-      try {
-        dispatch(editItem({ data, id: currentUserEdit?.id }))
-        toast.success('Venta editada correctamente')
-      } catch (error) {
-        console.log(error)
-      }
-    },
   }
 
-  const newUserModal = {
-    title: 'Agrega una nueva venta',
-    buttonTitle: 'Agregar',
-    ...modalInputs,
-    action: tableActions.create,
-  }
+  const handleSetCurrentIdEdit = (id: number) => dispatch(setCurrentItemId(id))
 
-  const editUserModal = {
-    title: 'Editar venta',
-    ...modalInputs,
-    action: tableActions.edit,
-  }
+  const dropdownAction: DropdownItemInteface[] = [
+    {
+      key: 'viewProducts',
+      title: 'Ver productos',
+      onPress: async (id) => {
+        handleSetCurrentIdEdit(id)
+      },
+      startContent: <MdShoppingCart />,
+    },
+    {
+      key: 'delete',
+      title: 'Borrar',
+      onPress: async (id) => {
+        try {
+          dispatch(deleteItem(id))
+          reqDeleteInvoice(id)
+        } catch (error: any) {
+          console.log(error)
+        }
+      },
+    },
+  ]
 
   return (
-    <AppTable
-      columnsData={columnsData}
-      tableActions={tableActions}
-      addItemModal={<AddItemModal modal={newUserModal} />}
-      editItemModal={<EditItemModal modal={editUserModal} />}
-    />
+    <>
+      <AppTable
+        columnsData={columnsData}
+        tableActions={tableActions}
+        dropdownAction={dropdownAction}
+      />
+      <ViewProductsModal />
+    </>
   )
 }
